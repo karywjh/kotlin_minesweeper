98e7b2d679b5708c9c48c0fba708d1ccb808ede3
karywjh <jiahuiw4@illinois.edu>
2020-05-01 20:48:28 -0700

    created and implemented Engine class

---
+++ sharedcode/src/main/java/com/example/sharedcode/Engine.kt
@@ -0,0 +1,52 @@
+package com.example.sharedcode
+
+class Engine {
+
+    enum class GameState {
+        kNotStarted,
+        kPlaying,
+        kWin,
+        kLose,
+    }
+
+    val board = Board()
+    var state = GameState.kNotStarted
+
+    fun startGame(row: Int, col: Int): Boolean {
+        if (this.board.cells[row][col].state != Cell.CellState.FLAGGED) {
+            // Start game when first cell was opened
+            this.board.GenerateBoard(Location(row, col))
+            this.state = GameState.kPlaying
+            return true
+        }
+
+        return false
+    }
+
+    fun openCell(row: Int, col: Int) {
+        if (this.board.cells[row][col].state != Cell.CellState.FLAGGED) {
+            this.board.cells[row][col].changeState(Cell.CellState.OPENED)
+            this.board.nonMines.remove(Location(row, col))
+
+            if (this.board.IsMine(Location(row, col))) {
+                this.state = GameState.kLose
+            }
+        }
+
+        if (this.board.nonMines.isEmpty()) {
+            this.state = GameState.kWin
+        }
+    }
+
+    fun flagCell(row: Int, col: Int) {
+        if (this.board.cells[row][col].state == Cell.CellState.FLAGGED) {
+            this.board.cells[row][col].changeState(Cell.CellState.COVERED)
+            this.board.mineCount++ // Total mine count increased by one
+
+        } else if (this.board.cells[row][col].state == Cell.CellState.COVERED) {
+            this.board.cells[row][col].changeState(Cell.CellState.FLAGGED)
+            this.board.mineCount-- // Total mine count decreased by one
+        }
+    }
+
+}
