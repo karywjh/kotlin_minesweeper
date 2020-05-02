ce012f4cc7c7c7cc0f6eb72a0a5f018d5c897e35
karywjh <jiahuiw4@illinois.edu>
2020-05-02 10:47:21 -0700

    set long-click to flag cells

--- sharedcode/src/main/java/com/example/sharedcode/Engine.kt
+++ sharedcode/src/main/java/com/example/sharedcode/Engine.kt
@@ -44,10 +44,12 @@ class Engine {
         if (this.board.cells[row][col].state == Cell.CellState.FLAGGED) {
             this.board.cells[row][col].changeState(Cell.CellState.COVERED)
             this.board.mineCount++ // Total mine count increased by one
+            println("unflagged")
 
         } else if (this.board.cells[row][col].state == Cell.CellState.COVERED) {
             this.board.cells[row][col].changeState(Cell.CellState.FLAGGED)
             this.board.mineCount-- // Total mine count decreased by one
+            println("flagged")
         }
     }
 
--- sharedcode/src/main/java/com/example/sharedcode/MainActivity.kt
+++ sharedcode/src/main/java/com/example/sharedcode/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sharedcode
 
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val gridView = this.findViewById(R.id.gridview) as GridView
         var adapter = CellAdapter(this, R.layout.chunk_cell, data)
         gridView.adapter = adapter
+
 
         gridView.onItemClickListener =
             OnItemClickListener { parent, v, position, id ->
@@ -33,6 +35,18 @@ class MainActivity : AppCompatActivity() {
                 adapter = CellAdapter(this, R.layout.chunk_cell, data)
                 gridView.adapter = adapter
             }
+
+        gridView.setOnItemLongClickListener { adapterView, view, i, l ->
+            val loc = engine.board.getLocation(i)
+            if (engine.state == Engine.GameState.kPlaying) {
+                engine.flagCell(loc.row, loc.col)
+                data[i] = CellLayout(engine.board.cells[loc.row][loc.col].imageFile)
+            }
+            adapter = CellAdapter(this, R.layout.chunk_cell, data)
+            gridView.adapter = adapter
+            true
+        }
+
     }
 
     val data: ArrayList<CellLayout>
